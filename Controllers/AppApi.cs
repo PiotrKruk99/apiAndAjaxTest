c14d7a4f34b2eece8689bbe509c283895498a8e8
PiotrKruk99-d <biuro@liberezo.pl>
2022-08-13 22:02:14 +0200

    Post and get queries work. Get method in two variants: with class and plain text sending.

--- Controllers/AppApi.cs
+++ Controllers/AppApi.cs
@@ -8,8 +8,29 @@ namespace homePage2.Controllers;
 public class AppApi : ControllerBase
 {
     [HttpGet("GetFormatedText")]
-    public string Get()
+    public string Get(string data1, string data2)
     {
-        return "tekst testowy zwracany przez api";
+        var returnData = data1.ToCharArray();
+        Array.Reverse(returnData);
+        return new String(returnData) + " | " + data2;
+    }
+
+    [HttpGet("FormatSingleText")]
+    public string GetSingle()
+    {
+        var data1 = Request.Query.Keys.ToArray<string>()[0];
+        var returnData = data1.ToCharArray();
+        Array.Reverse(returnData);
+        return new String(returnData);
+    }
+
+    [HttpPost("PostFormatedText")]
+    public string Post()
+    {
+        string data = Request.Form["text1"];
+        string data2 = Request.Form["text2"];
+        var returnData = data.ToCharArray();
+        Array.Reverse(returnData);
+        return new String(returnData) + " | " + data2;
     }
 }
